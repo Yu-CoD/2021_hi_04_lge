0a12b5073d25abea2f243f5c6b4a45eb8ee1736c
Yu-CoD <dbdnjs09@gmail.com>
2021-03-22 12:06:57 +0900

    link-wrapper완성

--- js/index.js
+++ js/index.js
@@ -4,11 +4,11 @@ $(".main-wrapper .pager-wrapper a.pager").click(function(){
 })
 
 $(".main-wrapper .pager-wrapper .bt-play").click(function(){
-	$(".main-wrapper .pager-wrapper .bt-play").addclass('active')
+	$(".main-wrapper .pager-wrapper .bt-play").addClass('active')
 	$(".main-wrapper .pager-wrapper .bt-pause").removeClass('active')
 })
 
-$(".main-wrapper .pager-wrapper .bt-play").click(function(){
+$(".main-wrapper .pager-wrapper .bt-pause").click(function(){
+	$(".main-wrapper .pager-wrapper .bt-play").removeClass('active')
 	$(".main-wrapper .pager-wrapper .bt-pause").addClass('active')
-	$(".main-wrapper .pager-wrapper .bt-play").removeclass('active')
 })
